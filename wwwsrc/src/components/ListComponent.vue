5ddae53e1f9d57f4ffc73527fdb966685baa0b4f
Monroe Gollaher <mgollaher92@gmail.com>
2020-12-26 22:21:23 -0700

    lists draw to page on FE

---
+++ wwwsrc/src/components/ListComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="list-component container-fluid">
+    <div class="row card shadow">
+      <h1>{{ list.title }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { AppState } from '../AppState'
+export default {
+  name: 'ListComponent',
+  props: ['listProp'],
+  setup(props) {
+    return {
+      list: computed(() => props.listProp),
+      profile: computed(() => AppState.profile)
+    }
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- wwwsrc/src/pages/ProfilePage.vue
+++ wwwsrc/src/pages/ProfilePage.vue
@@ -6,23 +6,28 @@
     <div class="row pb-3">
       <new-item-component />
     </div>
+    <div class="row pb-3">
+      <list-component v-for="list in lists" :key="list.id" :list-prop="list" />
+    </div>
   </div>
 </template>
 
 <script>
-import { computed } from 'vue'
+import { computed, onMounted } from 'vue'
 import { AppState } from '../AppState'
 import { listsService } from '../services/ListsService'
 export default {
   name: 'Profile',
   setup() {
+    onMounted(async() => {
+      await listsService.getLists()
+    })
     return {
       profile: computed(() => AppState.profile),
-      getLists() {
-        listsService.getLists()
-      }
+      lists: computed(() => AppState.lists)
     }
-  }
+  },
+  components: { }
 }
 </script>
 
